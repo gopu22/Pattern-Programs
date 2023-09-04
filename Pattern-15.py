1c06fe813391ea01adfafc14b604294606ecd607
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-09-04 16:24:02 +0530

    04-Sep-2023

--- Pattern-15.py
+++ Pattern-15.py
@@ -1,1 +1,14 @@
 
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    for j in range(n+1-i):
+        print('* ',end=' ')
+    print()
+
+
+#OUTPUT
+*  *  *  *  *
+*  *  *  *
+*  *  *
+*  *
+*
