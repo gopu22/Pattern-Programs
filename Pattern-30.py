d4fa97b316e322caaffadd853d382ae8a4306e2b
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-09-04 20:52:19 +0530

    04-Sep-2023

--- Pattern-30.py
+++ Pattern-30.py
@@ -1,1 +1,14 @@
 
+n=int(input("Enter the number: "))
+p=0
+for i in range(1,n+1):
+    print('  '*p,(str(n+1-i)+" ")*(n+1-i))
+    p+=1
+
+
+#OUTPUT
+ 5 5 5 5 5 
+   4 4 4 4 
+     3 3 3 
+       2 2 
+         1
