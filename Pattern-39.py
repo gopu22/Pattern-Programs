e298ae6479b4329baf21abbef674c61aadb35b1f
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-09-04 21:11:25 +0530

    04-Sep-2023

--- Pattern-39.py
+++ Pattern-39.py
@@ -1,1 +1,15 @@
 
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    print("  "*(n+1-i),end=' ')
+    for j in range(2*i-1,0,-1):
+        print(j,end=' ')
+    print()
+
+
+#OUTPUT
+           1 
+         3 2 1 
+       5 4 3 2 1 
+     7 6 5 4 3 2 1 
+   9 8 7 6 5 4 3 2 1
