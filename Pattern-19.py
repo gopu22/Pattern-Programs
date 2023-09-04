bc20460886d1acd3775c2b6081d60f9d16bb13a2
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-09-04 16:31:42 +0530

    04-Sep-2023

--- Pattern-19.py
+++ Pattern-19.py
@@ -1,1 +1,14 @@
 
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    for j in range(n+1-i):
+        print(chr(65+j),end=' ')
+    print()
+
+
+#OUTPUT
+A B C D E 
+A B C D 
+A B C 
+A B 
+A
