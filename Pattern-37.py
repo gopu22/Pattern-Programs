56ae6fb23840f0d348f156db46ee2918d32e65f0
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-09-04 21:07:09 +0530

    04-Sep-2023

--- Pattern-37.py
+++ Pattern-37.py
@@ -1,1 +1,12 @@
 
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    print('  '*(n-i),(chr(64+2*i-1)+" ")*(2*i-1))
+
+
+#OUTPUT
+         A 
+       C C C 
+     E E E E E 
+   G G G G G G G 
+ I I I I I I I I I
