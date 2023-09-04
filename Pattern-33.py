25c09d78109a77cce9c2cbd6273602373911e203
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-09-04 21:01:26 +0530

    04-Sep-2023

--- Pattern-33.py
+++ Pattern-33.py
@@ -1,1 +1,17 @@
 
+n=int(input("Enter the number: "))
+p=0
+for i in range(1,n+1):
+    print("  "*p,end=' ')
+    for j  in range(65,66+n-i):
+        print(chr(j),end=' ')
+    print()
+    p+=1
+
+
+#OUTPUT
+ A B C D E 
+   A B C D 
+     A B C 
+       A B 
+         A
