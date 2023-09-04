e0cf9a4293e22c20437fbacd1fd5eed134e7edca
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-09-04 17:39:11 +0530

    04-Sep-2023

--- Pattern-25.py
+++ Pattern-25.py
@@ -1,1 +1,17 @@
 
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    print(' '*(n-i),(str(i)+" ")*i)
+    print()
+
+
+#OUTPUT
+     1 
+
+    2 2 
+
+   3 3 3 
+
+  4 4 4 4 
+
+ 5 5 5 5 5
