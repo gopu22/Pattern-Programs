24fb359e3497c1cb7ea88ad0c7111960f27b2a1c
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-09-04 16:28:05 +0530

    04-Sep-2023

--- Pattern-16.py
+++ Pattern-16.py
@@ -1,1 +1,14 @@
 
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    for j in range(n+1-i):
+        print(i,end=' ')
+    print()
+
+
+#OUTPUT
+1 1 1 1 1 
+2 2 2 2 
+3 3 3 
+4 4 
+5
---
+++ Pattern-17.py
@@ -0,0 +1,14 @@
+
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    for j in range(1,n+2-i):
+        print(j,end=' ')
+    print()
+
+
+#OUTPUT
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1
