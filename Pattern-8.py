77074bb01b26e6890062937c860e39e75803306f
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-09-04 16:10:23 +0530

    04-Sep-2023

--- Pattern-8.py
+++ Pattern-8.py
@@ -1,1 +1,14 @@
 
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(chr(65+n-i),end=' ')
+    print()
+
+
+#OUTPUT
+E E E E E 
+D D D D D 
+C C C C C 
+B B B B B 
+A A A A A
