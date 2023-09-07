3968a480bf14b563a6a61d649ea52041d92267f9
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-09-07 22:11:33 +0530

    05-Sep-2023

--- Pattern-43.py
+++ Pattern-43.py
@@ -1,1 +1,17 @@
 
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    print(" "*(n-i),end=' ')
+    for j in range(1,i):
+        print(chr(65+(i-j)),end=' ')
+    for k in range(0,i):
+        print(chr(65+k),end=' ')
+    print()
+
+
+#OUTPUT
+     A 
+    B A B 
+   C B A B C 
+  D C B A B C D 
+ E D C B A B C D E
