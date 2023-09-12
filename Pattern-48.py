dc3235a534f61528e28f00a3591d2709c9b8b12a
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-09-12 23:12:48 +0530

    12-Sep-2023

--- Pattern-48.py
+++ Pattern-48.py
@@ -1,1 +1,17 @@
 
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    print(' '*(i-1),end=' ')
+    for j in range(0,n+1-i):
+        print(n+1-i,end=' ')
+    for k in range(1,n+1-i):
+        print(n+1-i,end=' ')
+    print()
+
+
+#OUTPUT
+ 5 5 5 5 5 5 5 5 5 
+  4 4 4 4 4 4 4 
+   3 3 3 3 3 
+    2 2 2 
+     1
