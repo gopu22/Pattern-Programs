afce226e10984eba61b1ec19e516839a34ee07e5
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-09-12 23:30:05 +0530

    12-Sep-2023

--- Pattern-52.py
+++ Pattern-52.py
@@ -1,1 +1,17 @@
 
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    print(' '*(i-1),end=' ')
+    for j in range(1,n+2-i,):
+        print(chr(65+2*n-2*i),end=' ')
+    for k in range(2,n+2-i):
+        print(chr(65+2*n-2*i),end=' ')
+    print()
+
+
+#OUTPUT
+ I I I I I I I I I 
+  G G G G G G G 
+   E E E E E 
+    C C C 
+     A
