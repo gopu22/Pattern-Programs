f3fdf38d1d597d059ed6dfe6e6461dfe15130c83
Govardhan Reddy B <govardhanbs2000@gmail.com>
2024-01-02 20:32:15 +0530

    02-Jan-2024

---
+++ Pattern-64.py
@@ -0,0 +1,23 @@
+
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(64+n+j-i),end=' ')
+    print()
+
+for k in range(1,n):
+    for l in range(1,n+1-k):
+        print(chr(64+l+k),end=' ')
+    print()
+
+
+#OUTPUT
+E 
+D E 
+C D E 
+B C D E 
+A B C D E 
+B C D E 
+C D E 
+D E 
+E 
