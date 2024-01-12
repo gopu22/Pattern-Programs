b2f4b0046a8a3164fce2d2a93ae4f798e3b2251a
Govardhan Reddy B <govardhanbs2000@gmail.com>
2024-01-12 23:40:35 +0530

    12-Jan-2024

--- Pattern-72.py
+++ Pattern-72.py
@@ -1,1 +1,15 @@
 
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    print(" "*(i-1),end=' ')
+    for j in range(1,n+2-i):
+        print(n+2-i-j,end=' ')
+    print()
+
+
+#OUTPUT
+ 5 4 3 2 1 
+  4 3 2 1 
+   3 2 1 
+    2 1 
+     1 
