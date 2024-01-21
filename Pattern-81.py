88f6fbd77c22bc3a34546f880a71e1b32d69d6c2
Govardhan Reddy B <govardhanbs2000@gmail.com>
2024-01-21 23:47:45 +0530

    21-Jan-2024

--- Pattern-81.py
+++ Pattern-81.py
@@ -1,1 +1,25 @@
 
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    print(" "*(n-i),end=' ')
+    for j in range(1,i+1):
+        print(chr(64+j),end=' ')
+    print()
+
+for k in range(1,n):
+    print(' '*k,end=' ')
+    for l in range(1,n+1-k):
+        print(chr(64+l+k),end=' ')
+    print()
+
+
+#OUTPUT
+     A 
+    A B 
+   A B C 
+  A B C D 
+ A B C D E 
+  B C D E 
+   C D E 
+    D E 
+     E 
