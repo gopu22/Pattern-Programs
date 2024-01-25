479caee39c5de1e84c1ff13616158135842c00f6
Govardhan Reddy B <govardhanbs2000@gmail.com>
2024-01-25 23:56:00 +0530

    25-Jan-2024

--- Pattern-85.py
+++ Pattern-85.py
@@ -1,1 +1,10 @@
 
+s1 = "adbc"
+s2 = "eiacdbhjikadbc"
+j=len(s1)
+for i in range(len(s2)-len(s1)+1):
+    if sorted(s1) == sorted(s2[i:j]):
+        print(True)
+        break
+    j+=1
+print(False)
