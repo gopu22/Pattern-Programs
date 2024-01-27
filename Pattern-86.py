4a4a91867f2fdf2b16ffd939a5188728f7a04e2b
Govardhan Reddy B <govardhanbs2000@gmail.com>
2024-01-27 22:10:52 +0530

    27-Jan-2024

--- Pattern-86.py
+++ Pattern-86.py
@@ -1,1 +1,19 @@
 
+n=int(input("Enter a number:"))
+for i in range(1,n+1):
+    print(" "*(n-i),end=' ')
+    for j in range(i,i+1):
+        print(n+1-i,end='')
+        if i>=2:
+            print(' '*(2*i-4),end=' ')
+            for k in range(i,i+1):
+                print(n+1-i,end=' ')
+    print()
+
+
+#OUTPUT
+     5
+    4 4 
+   3   3 
+  2     2 
+ 1       1 
