1f7bb9451b69e2c649ab1a0e9fcdd8ab702bc657
Govardhan Reddy B <govardhanbs2000@gmail.com>
2024-01-30 21:36:04 +0530

    30-Jan-2024

--- Pattern-89.py
+++ Pattern-89.py
@@ -1,1 +1,19 @@
 
+n=int(input("Enter a number:"))
+for i in range(1,n+1):
+    print(" "*(i-1),end=' ')
+    for j in range(i,i+1):
+        print('*',end='')
+        if i<=4:
+            print(' '*(2*n-2*i-2),end=' ')
+            for k in range(i,i+1):
+                print('*',end=' ')
+    print()
+
+
+#OUTPUT
+ *       * 
+  *     * 
+   *   * 
+    * * 
+     *
