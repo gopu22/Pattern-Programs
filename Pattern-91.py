40d8af22da71884ad6684ea09fcb7a55567373d0
Govardhan Reddy B <govardhanbs2000@gmail.com>
2024-02-02 23:52:09 +0530

    02-Feb-2024

--- Pattern-91.py
+++ Pattern-91.py
@@ -1,3 +1,19 @@
 
-a = [2,11,7,15]
-target = 9
+n=int(input("Enter a number:"))
+for i in range(1,n+1):
+    print(" "*(i-1),end=' ')
+    for j in range(i,i+1):
+        print(n+1-i,end='')
+        if i<=4:
+            print(' '*(2*n-2*i-2),end=' ')
+            for k in range(i,i+1):
+                print(n+1-i,end=' ')
+    print()
+
+
+#OUTPUT
+ 5       5 
+  4     4 
+   3   3 
+    2 2 
+     1 
