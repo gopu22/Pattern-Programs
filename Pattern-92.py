2addbaf3c90a911bf265668a58d922871d1a7a0d
Govardhan Reddy B <govardhanbs2000@gmail.com>
2024-02-06 22:04:43 +0530

    06-Feb-2024

--- Pattern-92.py
+++ Pattern-92.py
@@ -1,3 +1,19 @@
 
-l=[]
+n=int(input("Enter a number:"))
+for i in range(1,n+1):
+    print(' '*(i-1),end=' ')
+    for j in range(i,i+1):
+        print(chr(65+n-i),end='')
+        if i<=4:
+            print(" "*(2*n-2*i-2),end=' ')
+            for k in range(i,i+1):
+                print(chr(65+n-i),end='')
+    print()
 
+
+#OUTPUT
+ E       E
+  D     D
+   C   C
+    B B
+     A
