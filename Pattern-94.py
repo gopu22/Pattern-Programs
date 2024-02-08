7963214365ab1f290e1baa9aa4c693b5ba076a09
Govardhan Reddy B <govardhanbs2000@gmail.com>
2024-02-08 23:55:26 +0530

    08-Feb-2024

--- Pattern-94.py
+++ Pattern-94.py
@@ -1,1 +1,15 @@
 
+n=int(input("Enter a number:"))
+for i in range(1,n+1):
+    print(' '*(i-1),end=' ')
+    for j in range(1,n+1):
+        print("* ",end=' ')
+    print()
+
+
+#OUTPUT
+ *  *  *  *  *  
+  *  *  *  *  *  
+   *  *  *  *  *  
+    *  *  *  *  *  
+     *  *  *  *  *  
